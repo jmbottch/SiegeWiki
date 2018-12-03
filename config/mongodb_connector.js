88c25f6de663064dfe8644e3ff05e2e98e2f95d4
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2018-12-04 00:59:08 +0100

    Fixed another issue

--- config/mongodb_connector.js
+++ config/mongodb_connector.js
@@ -3,9 +3,9 @@ const config = require('./mongodb_config');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://app:Password1@ds061938.mlab.com:61938/siegewiki', { useNewUrlParser: true });
+mongoose.connect(config.dburl_env, { useNewUrlParser: true });
     var connection = mongoose.connection
-    .once('open', () => console.log('Connected to Mongo on ' + config.dburl))
+    .once('open', () => console.log('Connected to Mongo on ' + config.dburl_env))
     .on('error', (error) => {
         console.warn('Warning', error.toString());
     });
