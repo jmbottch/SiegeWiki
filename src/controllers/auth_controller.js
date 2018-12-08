d447812cbe3d79d395ae80c2472f649064d86ed0
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2018-12-08 15:09:42 +0100

    changed some auth

--- src/controllers/auth_controller.js
+++ src/controllers/auth_controller.js
@@ -29,16 +29,20 @@ module.exports = {
         });
     },
     
-     validateToken(req, res, next) {
-        var token = req.headers['x-access-token'];
-        console.log(token)
-        if (!token) return res.status(401).send({ Error :'No token provided.'})
-        
-        jwt.verify(token, config.secret, function(err, decoded) {
-            console.log(decoded)
-          if (err) return res.status(401).send({ Error :'Token is invalid.'})
-          if (decoded) next();
-        });
-    }
+     verifyToken(req, res, next) {
+         if(!req.headers.authorization){
+            return res.status(401).send('Unauthorized request')
+         }
+         let token = req.headers.authorization.split(' ')[1]
+         if (token === 'null') {
+            return res.status(401).send('Unauthorized request')
+         }
+         let payload = jwt.verify(token, config.secret)
+         if (!payload) {
+             return res.status(401).send('Unauthorized request')
+         }
+         req.userId = payload.subject
+         next()
+     }
     
 }
