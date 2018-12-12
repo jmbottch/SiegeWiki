1ba8eb8f88e94bbf391e1f02e44d3714fa87c27e
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2018-12-12 03:51:45 +0100

    never lose hope

--- src/controllers/season_controller.js
+++ src/controllers/season_controller.js
@@ -16,103 +16,84 @@ module.exports = {
             year: req.body.year,
             season: req.body.season
         })
-        .then(() => 
-            res.status(200).send({Message : "Season has been created."}),
-            console.log('<------SEASON HAS BEEN CREATED------>'))
+            .then(() =>
+                res.status(200).send({ Message: "Season has been created." }),
+                console.log('<------SEASON HAS BEEN CREATED------>'))
             .catch((err) => {
-            if (err.name == 'MongoError' && err.code == 11000)    
-            {
-                    res.status(401).send({ Error: 'This name has already been already taken.'});
-            } else 
-            {
-                    res.status(401).send({err});
-            }
-        });
+                if (err.name == 'MongoError' && err.code == 11000) {
+                    res.status(401).send({ Error: 'This name has already been already taken.' });
+                } else {
+                    res.status(401).send({ err });
+                }
+            });
     },
 
     edit(req, res) {
-        Season.findOne({name: req.body.name})
-        .then(season => {
-            if(season === null) {
-                res.status(401).send({Error: 'Season does not exist.'})
-            } else 
-            {
+        Season.findOne({ name: req.body.name })
+            .then(season => {
+                if (season === null) {
+                    res.status(401).send({ Error: 'Season does not exist.' })
+                } else {
 
-                season.set({
-                     'name': req.body.newName,
-                     'description': req.body.description,
-                     'year': req.body.year,
-                     'season' : req.body.season
-                })    
-                season.save()
-                .then(() => res.status(200).send({Message: "Season has been edited."}))
-                .catch((err) => res.status(401).send({err}));    
-            }
-        });
+                    season.set({
+                        'name': req.body.newName,
+                        'description': req.body.description,
+                        'year': req.body.year,
+                        'season': req.body.season
+                    })
+                    season.save()
+                        .then(() => res.status(200).send({ Message: "Season has been edited." }))
+                        .catch((err) => res.status(401).send({ err }));
+                }
+            });
 
     },
 
-     recreate(res, season, operatorAdd, mapAdd) {
-        console.log(season._id + season)
-        Season.findOne({ name: season.name })
-        .then((foundSeason) => {
-            foundSeason.delete()
+
+
+    populate(req, res, mapAdd, operatorAdd) {
+        let operatorName = req.body.name;
+        let worldName = req.body.name;
+        let foundOperator = new Operator();
+        let foundMap = new Map();
+
+
+        Operator.findOne({ name: operatorName })
+            .then(opResult => {
+                foundOperator = opResult;
+            })
+            .catch((err) => res.status(401).send({ err }));
+
+        World.findOne({ name: worldName })
+            .then(mapResult => {
+                foundMap = mapResult;
+            })
             .then(() => {
                 Season.create({
-                    _id: season._id,
-                    _v: season._v,
-                    name: season.name,
-                    description: season.description,
-                    year: season.year,
-                    season: season.sesaon,
+                    name: req.body.name,
+                    description: req.body.description,
+                    year: req.body.year,
+                    season: req.body.season,
                     operator: operatorAdd,
                     world: mapAdd
-                }).then(() => { res.status(200).send({Message: "Populated season succesfully"}) })
+                }).then(() => { res.status(200).send({ Message: "Populated season succesfully" }) })
             })
-        })
-    },
-    
-     populate(req, res) {
-        Season.findOne( { name: req.body.name } )
-        .then(season => {
-            if(season === null){
-                res.status(401).send({ Error :'Season does not exist.'})
-            }
-            else { 
-                let operatorName = req.body.name;
-                let worldName = req.body.name;
-                let foundOperator = new Operator();
-                let foundMap = new Map();
-                
-    
-                Operator.findOne({ name: operatorName })
-                    .then(opResult => {
-                        foundOperator = opResult;
-                    })
-                    .catch((err) => res.status(401).send({err}));
-    
-                    World.findOne({ name: worldName })
-                    .then(mapResult => {
-                        foundMap = mapResult;
-                    })
-                    .catch((err) => res.status(401).send({err}));
-                    recreate(res, season, foundOperator, foundMap)
-            }
-        });
-    },
 
-    delete(req, res) {
-        Season.findOne({name: req.headers.name})
-        .then(season => {
-            if(season === null) {
-                res.status(401).send({Error: 'Season does not exist.'})
-            }
-            else
-            {
-                season.delete()
-                .then(() => res.status(200).send({Message: "Season has been deleted."}));
-            }
-        });
+
+            .catch((err) => res.status(401).send({ err }));
+        },
+
+        delete (req, res) {
+            Season.findOne({ name: req.headers.name })
+                .then(season => {
+                    if (season === null) {
+                        res.status(401).send({ Error: 'Season does not exist.' })
+                    }
+                    else {
+                        season.delete()
+                            .then(() => res.status(200).send({ Message: "Season has been deleted." }));
+                    }
+                });
+        }
+
     }
-        
-}
