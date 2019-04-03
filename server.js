0f3b9fa5637a95703d70d883f3d6c31442fef0b5
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2019-04-03 21:19:40 +0200

    didnt work

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const routes = require('./routes/routes');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
-app.use(cors({
-  origin: ['localhost:4200','http://siegewikia.herokuapp.com']
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 routes (app);
